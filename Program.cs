e9e55f9c88adaf9a05331ca4f58048695a9978ef
sadashiva1997 <sadashivamoji1997@gmail.com>
2022-07-28 11:52:34 +0530

    my first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum); // Print the sum of x + y
+        }
+    }
+}
